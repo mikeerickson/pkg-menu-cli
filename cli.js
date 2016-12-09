473e8eaeaa929de7d2c81a72f85ae820e4e26b6b
Mike Erickson <merickson@cirruspath.com>
2016-12-09 14:38:53 -0800

    Initial Commit

---
+++ cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+'use strict';
+
+const meow        = require('meow');
+const packageMenu = require('./src/packageMenu.js');
+
+const cli = meow(`
+    Usage
+      $ package-menu [options]
+
+    Options
+      --sort, -s  Sort Scripts (default: false)
+
+    Examples
+      $ package-menu --sort
+`, {
+    alias: {
+        s: 'sort'
+    }
+});
+
+// 'input: ', cli.input[0];
+// console.log('flags', cli.flags);
+
+packageMenu.build(cli.flags)
+
+
+// foo(cli.input[0], cli.flags);
---
+++ src/packageMenu.js
@@ -0,0 +1,40 @@
+const chalk   = require('chalk');
+const pkgInfo = require('../package.json');
+const Table   = require('cli-table2');         // INFO: https://www.npmjs.com/package/cli-table2
+
+function buildMenu(opts = {}) {
+  // instantiate
+  let table = new Table({
+      head: ['Name', 'Script'],
+      colWidths: [50, 50]
+  });
+
+  let scripts = pkgInfo.scripts;
+  const scriptNames = Object.keys(scripts);
+  if (opts.sort) {
+    scriptNames.sort();
+  }
+  scriptNames.map((item) => {
+    table.push(
+        [item, scripts[item]]
+    );
+  });
+
+  console.log('');
+  console.log(chalk.white.cyan(pkgInfo.name) + ' ' + chalk.white.bold(pkgInfo.version));
+
+  console.log(table.toString());
+
+  return scripts;
+}
+
+
+let packageMenu = {
+  build: (options) => {
+    return buildMenu(options);
+  }
+}
+
+
+
+module.exports = packageMenu;
