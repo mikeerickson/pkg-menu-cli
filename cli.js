0eb9aedb8f49a96420529daa2ac77a41434dd2d8
Mike Erickson <merickson@cirruspath.com>
2016-12-14 10:50:54 -0800

    added cli updater

--- cli.js
+++ cli.js
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
 'use strict';
 
-const meow        = require('meow');
-const packageMenu = require('./src/packageMenu.js');
+const meow           = require('meow');
+const execa          = require('execa');
+const updateNotifier = require('update-notifier');
+const pkg            = require('./package.json');
+const packageMenu    = require('./src/packageMenu.js');
+
+updateNotifier({pkg}).notify();
 
 const cli = meow(`
     Usage
       $ package-menu [options]
 
     Options
-      --help, -h      Shows Help (this screen)
-      --sort, -s      Sort Scripts (default: false)
-      --version, -V   Show Package Menu Version
+      --help, -h         Shows Help (this screen)
+      --sort, -s         Sort Scripts (default: false)
+      --version, -V, -v  Show Package Menu Version
 
     Examples
       $ package-menu --sort
@@ -19,8 +24,16 @@ const cli = meow(`
     alias: {
         s: 'sort',
         h: 'help',
-        V: 'version'
+        V: 'version',
+        v: 'version'
     }
 });
 
-packageMenu.print('', cli.flags)
+if (cli.flags.update) {
+  execa('npm i -g package-menu-cli@latest', []).then(result => {
+    console.log(result.stdout);
+});
+}
+else {
+  packageMenu.print('', cli.flags)
+}
